43d67c4007d20db113a09e65ab548ed923c5c40e
banchet18 <banjarac6@gmail.com>
2024-01-12 12:18:42 +0530

    c

--- vite-project/src/pages/Home.jsx
+++ vite-project/src/pages/Home.jsx
@@ -1,4 +1,10 @@
+import { useState } from "react";
+
 const Home = () => {
+  const [value, setValue] = useState();
+
+  console.log(value);
+
   return (
     <div
       style={{
@@ -35,8 +41,13 @@ const Home = () => {
             type="text"
             placeholder="Add to Do"
             style={{ width: "70%", height: "50px", fontSize: "20px" }}
+            onChange={(e) => {
+              setValue(e.target.value);
+            }}
           />
-          <button style={{ width: "30%", height: "50px" }}>Add</button>
+          <button onClick={() => {}} style={{ width: "30%", height: "50px" }}>
+            Add
+          </button>
         </div>
         <div
           style={{ border: "2px solid green", width: "70%", height: "50%" }}
